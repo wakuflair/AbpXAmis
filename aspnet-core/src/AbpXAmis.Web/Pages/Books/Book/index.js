e9244e91d176a5db70252e6b55f7295a5d4803d8
wakuflair <wakuflair@163.com>
2020-08-18 10:12:20 +0800

    增加注释说明, 优化JSON

--- aspnet-core/src/AbpXAmis.Web/Pages/Books/Book/index.js
+++ aspnet-core/src/AbpXAmis.Web/Pages/Books/Book/index.js
@@ -1,37 +1,38 @@
 $(function () {
 
-    var l = abp.localization.getResource('AbpXAmis');
-    var service = abpXAmis.books.book;
+    var l = abp.localization.getResource("AbpXAmis");
 
-    let indexJson = {
-        "type": "page",
-        "title": l("Book"),
-        "toolbar": [
+    let amisJson = {
+        type: "page",
+        title: l("Book"),
+        toolbar: [
             {
-                "type": "button",
-                "label": l("CreateBook"),
-                "actionType": "dialog",
-                "dialog": {
-                    "title": "系统提示",
-                    "body": "对你点击了"
+                type: "button",
+                label: l("CreateBook"),
+                actionType: "dialog",
+                dialog: {
+                    title: "系统提示",
+                    body: "对你点击了"
                 },
-                "icon": "fa fa-plus",
-                "iconClassName": "pull-left"
+                icon: "fa fa-plus",
+                iconClassName: "pull-left"
             }
         ],
-        "body": {
-            "type": "crud",
-            "api": {
-                "url": "/api/app/book",
-                "method": "post",
-                "requestAdaptor": function (api) {
+        body: {
+            type: "crud",
+            api: {
+                url: "/api/app/book",
+                method: "post",   // 临时使用post, 下面再改回get, 原因: https://github.com/baidu/amis/issues/853
+                requestAdaptor: api => {
                     let sorting = "";
                     if (api.data.orderBy) {
-                       sorting =  `${api.data.orderBy} ${api.data.orderDir}`;
+                        // 拼接ABP排序字符串, 例:name asc 或 price desc
+                        sorting = `${api.data.orderBy} ${api.data.orderDir}`;
                     }
                     return {
                         ...api,
                         method: "get",
+                        // 构造ABP GetList方法需要的参数
                         data: {
                             skipCount: (api.data.page - 1) * api.data.perPage,
                             maxResultCount: api.data.perPage,
@@ -39,11 +40,13 @@ $(function () {
                         }
                     };
                 },
-                "adaptor": function (payload, response) {
+                adaptor: (payload, response) => {
+                    // 处理书籍类型的本地化
                     let items = payload.items;
                     items.forEach(item => {
                         item.type = l(`Enum:BookType:${item.type}`);
                     });
+                    // 转换ABP GetList返回的结果为amis需要的结构
                     return {
                         status: response.status === 200 ? 0 : response.status,
                         data: {
@@ -53,43 +56,43 @@ $(function () {
 
                     };
                 },
-                "replaceData": true
+                replaceData: true
             },
-            "columns": [
+            columns: [
                 {
-                    "name": "name",
-                    "label": l("BookName"),
-                    "sortable": true
+                    name: "name",
+                    label: l("BookName"),
+                    sortable: true
                 },
                 {
-                    "name": "type",
-                    "label": l("BookType"),
-                    "sortable": true,
+                    name: "type",
+                    label: l("BookType"),
+                    sortable: true,
                 },
                 {
-                    "name": "publishDate",
-                    "label": l("BookPublishDate"),
-                    "sortable": true,
-                    "tpl": "${publishDate | date:LL:YYYY-MM-DD}"
+                    name: "publishDate",
+                    label: l("BookPublishDate"),
+                    sortable: true,
+                    tpl: "${publishDate | date:LL:YYYY-MM-DD}"    // `LL`指定输出日期格式(本地日期格式), `YYYY-MM-DD`指定输入日期格式
                 },
                 {
-                    "name": "price",
-                    "label": l("BookPrice"),
-                    "sortable": true,
-                    "tpl": "${price | round:2}"
+                    name: "price",
+                    label: l("BookPrice"),
+                    sortable: true,
+                    tpl: "${price | round:2}"
                 },
                 {
-                    "name": "creationTime",
-                    "label": l("BookCreationTime"),
-                    "sortable": true,
-                    "tpl": "${creationTime | date:LL:YYYY-MM-DD}"
+                    name: "creationTime",
+                    label: l("BookCreationTime"),
+                    sortable: true,
+                    tpl: "${creationTime | date:LL:YYYY-MM-DD}"
                 },
             ]
         }
     };
 
     (function () {
-        let amis = amisRequire('amis/embed');
-        let amisScoped = amis.embed('#root', indexJson);
+        let amis = amisRequire("amis/embed");
+        let amisScoped = amis.embed("#root", amisJson);
     })();
 });
